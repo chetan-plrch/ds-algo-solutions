d3ad2e66ce0d170a797acc21a8c8c0f7cc3b2ea0
Chetan Jain <chetanplrch@gmail.com>
2021-08-06 18:21:49 +0530

    Construct tree using inorder and preorder or postorder traversal

--- CodingNinjas/Tree/constructUsingTraversal.js
+++ CodingNinjas/Tree/constructUsingTraversal.js
@@ -11,15 +11,12 @@ const constructTree = (preOrder, inOrder) => {
 }
 
 const traverseTree = (rootEle, preOrder, inOrder, inOrderMin, inOrderMax, inOrderMap) => {
-    if(rootEle) {
+    if (rootEle) {
         const root = new BinaryTreeNode(rootEle);
 
-        if(inOrderMin >= inOrderMax) {
+        if (inOrderMin >= inOrderMax) {
             return root;
         }
-
-        const lInorder = inOrder.slice(inOrderMin, inOrderMap[rootEle]);
-        const rInorder = inOrder.slice(inOrderMap[rootEle] + 1, inOrderMax);
 
         const lRoot = preOrder.find((ele) => ((inOrderMap[ele] >= inOrderMin) && (inOrderMap[ele] < inOrderMap[rootEle])));
         const rRoot = preOrder.find((ele) => ((inOrderMap[ele] > inOrderMap[rootEle]) && (inOrderMap[ele] <= inOrderMax)));
@@ -27,7 +24,7 @@ const traverseTree = (rootEle, preOrder, inOrder, inOrderMin, inOrderMax, inOrde
         root.left = traverseTree(
             lRoot,
             preOrder,
-            lInorder,
+            inOrder,
             inOrderMin,
             inOrderMap[rootEle] - 1,
             inOrderMap
@@ -36,17 +33,83 @@ const traverseTree = (rootEle, preOrder, inOrder, inOrderMin, inOrderMax, inOrde
         root.right = traverseTree(
             rRoot,
             preOrder,
-            rInorder,
+            inOrder,
             inOrderMap[rootEle] + 1,
             inOrderMax,
             inOrderMap
         );
 
-        if(binaryTree.isLeafNode(root))
+        if (binaryTree.isLeafNode(root))
             return root;
         return root;
     }
     return null;
+}
+
+const constructTreeUsingPreOrder = (preOrder, inOrder) => {
+    const inOrderMap = inOrder.reduce((acc, cur, idx) => {
+        acc[cur] = idx;
+        return acc;
+    }, {});
+
+    const pIndex = [0];
+
+    return traverseTreeUsingPreOrder(0, inOrder.length - 1, inOrderMap, pIndex, preOrder);
+}
+
+const traverseTreeUsingPreOrder = (startIndex, endIndex, inOrderMap, pIndex, preOrder) => {
+    if (startIndex > endIndex) {
+        return null;
+    }
+
+    const rootEle = preOrder[pIndex[0]];
+    const root = new BinaryTreeNode(rootEle);
+
+    pIndex[0] = pIndex[0] + 1;
+
+    if (startIndex === endIndex) {
+        return root;
+    } else {
+        const inOrderIndex = inOrderMap[root.val];
+
+        root.left = traverseTreeUsingPreOrder(startIndex, inOrderIndex - 1, inOrderMap, pIndex, preOrder);
+        root.right = traverseTreeUsingPreOrder(inOrderIndex + 1, endIndex, inOrderMap, pIndex, preOrder);
+
+        return root;
+    }
+}
+
+const constructTreeUsingPostOrder = (postOrder, inOrder) => {
+    const inOrderMap = inOrder.reduce((acc, cur, idx) => {
+        acc[cur] = idx;
+        return acc;
+    }, {});
+
+    const pIndex = [inOrder.length - 1];
+
+    return traverseTreeUsingPostOrder(0, inOrder.length - 1, inOrderMap, pIndex, postOrder);
+}
+
+const traverseTreeUsingPostOrder = (startIndex, endIndex, inOrderMap, pIndex, postOrder) => {
+    if (startIndex > endIndex) {
+        return null;
+    }
+
+    const rootEle = postOrder[pIndex[0]];
+    const root = new BinaryTreeNode(rootEle);
+
+    pIndex[0] = pIndex[0] - 1;
+
+    if (startIndex === endIndex) {
+        return root;
+    } else {
+        const inOrderIndex = inOrderMap[root.val];
+
+        root.right = traverseTreeUsingPostOrder(inOrderIndex + 1, endIndex, inOrderMap, pIndex, postOrder);
+        root.left = traverseTreeUsingPostOrder(startIndex, inOrderIndex - 1, inOrderMap, pIndex, postOrder);
+    
+        return root;
+    }
 }
 
 const example = {
@@ -61,12 +124,15 @@ const example2 = {
 
 const example3 = {
     preOrder: [67, 98, 23, 3, 9, 45, 1, 12, 2, 4, 90, 99, 26, 25, 55, 24, 7],
-    inOrder: [9, 3, 45, 23, 1, 98, 2, 12, 4, 67, 26, 99, 25, 90, 24, 55, 7]
+    inOrder: [9, 3, 45, 23, 1, 98, 2, 12, 4, 67, 26, 99, 25, 90, 24, 55, 7],
+    postOrder: [9, 45, 3, 1, 23, 2, 4, 12, 98, 26, 25, 99, 24, 7, 55, 90, 67]
 }
 
 const b1 = new BinaryTree();
-b1.setRootNode(constructTree(example3.preOrder, example3.inOrder));
+// b1.setRootNode(constructTreeUsingPreOrder(example3.preOrder, example3.inOrder));
+b1.setRootNode(constructTreeUsingPostOrder(example3.postOrder, example3.inOrder));
 
 b1.traverseLevelOrder();
+// b1.traversePostOrder();
 
 console.log(b1.getRootNode());
