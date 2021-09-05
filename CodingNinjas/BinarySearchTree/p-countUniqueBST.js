c481ae9fbec1afe9fcc13e0581eaa07b90470e84
Chetan Jain <chetan.p.jain@lowes.com>
2021-09-05 20:26:51 +0530

    Add the catalan number code with memoization

--- CodingNinjas/BinarySearchTree/p-countUniqueBST.js
+++ CodingNinjas/BinarySearchTree/p-countUniqueBST.js
@@ -1,7 +1,32 @@
-const a = [1, 4, 6, 8, 89, 90];
+/**
+ * Get the catalan number for BST
+ */
+const a = new Array(20);
 
 const countUniqueBST = (arr) => {
-    
+    const f = new Array(arr.length + 1).fill(0);
+    f[0] = 1;
+    f[1] = 1;
+    return calculate(arr.length, f);
 }
 
+const calculate = (size, f) => {
+    if (size === 0 || size === 1)
+        return f[0];
+    else if(f[size] !== 0)
+        return f[size];
+    else { 
+        let i = 0;
+        let total = 0;
+        while(i < size) {
+            total = total + (calculate(i, f) * calculate(size - i - 1, f));
+            i++;
+        }
+        f[size] = total;
+        return f[size];
+    }
+}
+
+console.time("dbsave");
 console.log('Count of unique BST', countUniqueBST(a));
+console.timeEnd("dbsave");
